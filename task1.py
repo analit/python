0c1846a6fcbd6bc9c7fab85fb59beacedbd5377e
s.garyaga@lucky-labs.com <you@example.com>
2017-10-13 13:10:38 +0300

    first commit

--- task1.py
+++ task1.py
@@ -14,8 +14,11 @@ print listFromTuple, type(listFromTuple)
 dictionaryFromList = dict((str(element), len(listFromTuple)) for element in listFromTuple)
 print dictionaryFromList, type(dictionaryFromList)
 
+testDict = dict([[1, 2], [2, 3]])
+print(testDict)
+
 dictionaryFromList = None
-dictionaryFromList = {str(element): len(listFromTuple) for element in listFromTuple}
+dictionaryFromList = dict([str(element), len(listFromTuple)] for element in listFromTuple)
 print dictionaryFromList, type(dictionaryFromList)
 
 dictionaryFromList = None
@@ -24,4 +27,3 @@ print dictionaryFromList, type(dictionaryFromList)
 
 dictionaryFromTuple = dict((str(element), tupleFromList.count(element)) for element in tupleFromList)
 print(dictionaryFromTuple, type(dictionaryFromTuple))
-
