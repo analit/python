da44a5f58d7788d0c2ce14e32d728714bd3a76ff
s.garyaga@lucky-labs.com <you@example.com>
2017-10-13 12:54:48 +0300

    first commit

---
+++ task1.py
@@ -0,0 +1,27 @@
+listObj = [1, 2, 3, 'owe', 'two', [3, 'three']]
+print listObj, type(listObj)
+
+tupleFromList = tuple(listObj)
+print tupleFromList, type(tupleFromList)
+
+listFromTuple = list(tupleFromList)
+print listFromTuple, type(listFromTuple)
+
+listFromTuple = None
+listFromTuple = [element for element in tupleFromList]
+print listFromTuple, type(listFromTuple)
+
+dictionaryFromList = dict((str(element), len(listFromTuple)) for element in listFromTuple)
+print dictionaryFromList, type(dictionaryFromList)
+
+dictionaryFromList = None
+dictionaryFromList = {str(element): len(listFromTuple) for element in listFromTuple}
+print dictionaryFromList, type(dictionaryFromList)
+
+dictionaryFromList = None
+dictionaryFromList = {str(element): len(listFromTuple) for element in listFromTuple}
+print dictionaryFromList, type(dictionaryFromList)
+
+dictionaryFromTuple = dict((str(element), tupleFromList.count(element)) for element in tupleFromList)
+print(dictionaryFromTuple, type(dictionaryFromTuple))
+
